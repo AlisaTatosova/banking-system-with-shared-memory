f859e3ad4233d57bb133debf08c4918c0cc18206
AlisaTatosova <140841890+AlisaTatosova@users.noreply.github.com>
2023-07-29 19:54:54 +0400

    Add files via upload

---
+++ banking-system/deinitializer.cpp
@@ -0,0 +1,34 @@
+//deinitializer destroys bank
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include "account.cpp"
+
+void deinitializer() {
+    key_t key = ftok(".", 'B');
+    if (key == -1) {
+        perror("ftok");
+        exit(1);
+    }
+    // Get the shared memory segment ID
+    int shmid = shmget(key, 0, 0);
+    if (shmid == -1) {
+        perror("shmget");
+        exit(1);
+    }
+    // Detach shared memory segment
+    shmdt((Account*)shmat(shmid, nullptr, 0));
+
+    //delete
+    if (shmctl(shmid, IPC_RMID, nullptr) == -1) {
+        perror("shmctl");
+        exit(1);
+    }
+
+    std::cout << "Bank destroyed" << std::endl;
+
+}
+
+int main() {
+    deinitializer();
+}
